f79d8c9768c8490c0731c981c770bdd6fe6d77ec
Dr. S. K. Choudhary <saket_au@rediffmail.com>
2023-04-06 14:10:04 +0530

    updated code

--- module3/StringOperations.java
+++ module3/StringOperations.java
@@ -11,14 +11,14 @@ public static void main(String[] args)
    	System.out.print("Enter the second String: ");
    	String str2 = reader.nextLine();
 
-	System.out.println("Upper case of string 1 is: " + str1.toUpperCase());//SACHIN    
-	System.out.println("Lower case of string 2 is: " + str2.toLowerCase());//sachin    
+	System.out.println("Upper case of string 1 is: " + str1.toUpperCase());
+	System.out.println("Lower case of string 2 is: " + str2.toLowerCase());
 	System.out.println("Str1 is uincahnged as : " + str1);
 
 	System.out.print("Enter the String which has spaces: ");
    	String str3 = reader.nextLine();
    	System.out.println("Str3 after trimming is : " + str3.trim());
-   	System.out.println("Str1 has character at 3rd place as : " + str1.charAt(0));
+   	System.out.println("Str1 has character at 3rd place as : " + str1.charAt(2));
    	System.out.println("Length of the Str1 is: " + str1.length());
 }  
 }  
