45e4aa7d1359867f586c291de8662dd613e02d8e
SaketJNU <saket_au@rediffmail.com>
2023-12-16 10:14:17 +0530

    class name vs file name

---
+++ module1/Test.java
@@ -0,0 +1,13 @@
+//Class Name and File Name may b e same if the class is a public class
+//If the class is not a public class, then it's may have different names
+// During compilation, file name will be used as javac Test.java
+// For running class name will be used as java Hello
+
+
+class Hello
+{
+  public static void main(String args[])
+  {
+     System.out.print("Hello World");
+  }
+}
