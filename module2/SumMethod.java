531a1b508b556848e221b635732a7a16c53ecb71
SaketJNU <saket_au@rediffmail.com>
2023-12-21 11:02:27 +0530

    updated code

--- module2/SumMethod.java
+++ module2/SumMethod.java
@@ -14,7 +14,7 @@ public class SumMethod
         int sum = sum2numbers(num1, num2);
         System.out.print("\nSum = "+sum);
    }
-   public static int sum2numbers(int n1, int n2)
+  static int sum2numbers(int n1, int n2)
    {
         return n1 + n2;
    }
