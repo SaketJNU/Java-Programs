29ce5f49ce0009137dbe273c37337822c0b1e63e
SaketJNU <saket_au@rediffmail.com>
2024-12-16 10:22:38 +0530

    update test.java file

--- module1/Test.java
+++ module1/Test.java
@@ -1,4 +1,4 @@
-//Class Name and File Name may b e same if the class is a public class
+//Class Name and File Name may be same if the class is a public class
 //If the class is not a public class, then it's may have different names
 // During compilation, file name will be used as javac Test.java
 // For running class name will be used as java Hello
