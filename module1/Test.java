604a952012c0d9ca804ca2d3f3cf3712b2208c86
SaketJNU <saket_au@rediffmail.com>
2024-12-24 13:29:27 +0530

    readme update

--- module1/Test.java
+++ module1/Test.java
@@ -1,7 +1,7 @@
 //Class Name and File Name may be same if the class is a public class
 //If the class is not a public class, then it's may have different names
 // During compilation, file name will be used as javac Test.java
-// For running class name will be used as java Hello
+// For running class name will be used as "java Hello" command as Hello is a class file (Bytecode). 
 
 
 class Hello
