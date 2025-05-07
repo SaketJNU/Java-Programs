e7b6f1269af1111a62177e6d52de4dcba8f31539
SaketJNU <saket_au@rediffmail.com>
2025-05-07 15:05:08 +0530

    Dog

---
+++ ucer/4B4/Animals.java
@@ -0,0 +1,44 @@
+class Animal {
+    // Instance variable
+    String name;
+
+    // Method to make a generic sound
+    public void makeSound() {
+        System.out.println("Some sound...");
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+}
+
+class Dog extends Animal {
+    // Overriding the makeSound method
+    @Override
+    public void makeSound() {
+        System.out.println("Bark! Bark!");
+    }
+}
+
+
+public class Animals {
+    public static void main(String[] args) {
+        // Create an instance of Dog
+        Dog myDog = new Dog();
+
+        // Set the dog's name
+        myDog.setName("Buddy");
+
+        // Call makeSound()
+        myDog.makeSound();
+
+        // Print the dog's name
+        System.out.println("Dog's name is: " + myDog.getName());
+    }
+}
