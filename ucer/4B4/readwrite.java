606cfd5d3263455bf62ef470b5d7af78b33fee55
SaketJNU <saket_au@rediffmail.com>
2025-05-21 13:56:27 +0530

    3 Programs

---
+++ ucer/4B4/readwrite.java
@@ -0,0 +1,27 @@
+// Java program for try-with-resources having multiple resources
+
+import java.io.*;
+
+class readwrite {
+    public static void main(String[] args)
+    {
+        try (FileOutputStream f
+             = new FileOutputStream("outputfile.txt");
+           BufferedReader br = new BufferedReader(
+                 new FileReader("hello.txt"))) {
+            String text;
+            while ((text = br.readLine()) != null) {
+                byte arr[] = text.getBytes();
+                f.write(arr);
+          }
+
+         System.out.println(
+                "File content copied to another one.");
+        }
+    catch (Exception e) {
+            System.out.println(e);
+        }
+       System.out.println(
+            "Resource are closed and message has been written into the file.");
+    }
+}
